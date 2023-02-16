991bed116c29e81f9e3a2f1532a50445be039ec8
max <laterty@gmail.com>
2023-02-16 17:58:54 +0200

    define expense factory

---
+++ spec/factories/expenses.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :expense do
+    name { FFaker::BaconIpsum.word }
+    value { rand(0..100.0) }
+    description { FFaker::BaconIpsum.sentence(10) }
+    user { User.first }
+    category { Category.all.sample }
+  end
+end
