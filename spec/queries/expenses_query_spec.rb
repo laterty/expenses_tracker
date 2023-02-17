236115e5a4a1054d1c377bb40c4290fd9c8d6c5b
max <laterty@gmail.com>
2023-02-17 19:14:51 +0200

    expenses query specs

---
+++ spec/queries/expenses_query_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+RSpec.describe ExpensesQuery do
+  subject(:query_result) { described_class.query(params) }
+
+  let!(:categories) { create_list(:category, 2) }
+
+  context 'expenses' do
+    context 'sorting only' do
+      let(:expense1) { create(:expense, created_at: Time.now.to_i.in_milliseconds) }
+      let(:expense2) { create(:expense, created_at: 2.minutes.ago.to_i.in_milliseconds) }
+      let(:expense3) { create(:expense, created_at: 5.minutes.ago.to_i.in_milliseconds) }
+
+      context 'when empty params' do
+        it 'sorts by default sort type (created_at asc)' do
+          expect(query_result).to eq([expense3, expense2, expense1])
+        end
+      end
+
+      context 'when sort params' do
+        let(:params) { { sort: 'created_at_desc' } }
+
+        it 'sorts by default created_at desc' do
+          expect(query_result).to eq([expense1, expense2, expense3])
+        end
+      end
+    end
+
+    context 'filtering only' do
+      context 'by category' do
+        let(:expense1) { create(:expense, category: categories[0]) }
+        let(:expense2) { create(:expense, category: categories[1]) }
+        let(:expense3) { create(:expense, category: categories[0]) }
+
+        context 'when filter second category' do
+          let(:params) { { category: categories[1] } }
+
+          it 'returns expenses only with second category' do
+            expect(query_result).to match_array([expense2])
+          end
+        end
+
+        context 'when filter by first category' do
+          let(:params) { { category: categories[0] } }
+
+          it 'returns expenses only with first category' do
+            expect(query_result).to match_array([expense1, expense3])
+          end
+        end
+      end
+    end
+
+    context 'sorting & filtering' do
+      let(:expense1) { create(:expense, created_at: Time.now.to_i.in_milliseconds, category: categories[0]) }
+      let(:expense2) do
+        create(:expense, created_at: 2.minutes.ago.to_i.in_milliseconds, category: categories[0])
+      end
+      let(:expense3) do
+        create(:expense, created_at: 5.minutes.ago.to_i.in_milliseconds, category: categories[1])
+      end
+
+      context 'filter by category first and sort by created_at desc' do
+        let(:params) do
+          { sort: 'created_at_desc', category: categories[0] }
+        end
+
+        it 'sorts by created_at desc and filter by category first' do
+          expect(query_result).to eq([expense1, expense2])
+        end
+      end
+
+      context 'filter by category first and sort by created_at asc' do
+        let(:params) do
+          { sort: 'created_at_asc', category: categories[0] }
+        end
+
+        it 'sorts by created_at desc and filter by category first' do
+          expect(query_result).to eq([expense2, expense1])
+        end
+      end
+    end
+  end
+end
