1a4eb1c5dcea51ae536786cbada58aa97240086d
max <laterty@gmail.com>
2023-02-17 16:06:53 +0200

    add form for expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,4 +5,27 @@ class ExpensesController < ApplicationController
   def index
     @expenses = current_user.expenses
   end
+
+  def show
+    @expense = current_user.expenses.find(params[:id])
+  end
+
+  def new
+    @expense = current_user.expenses.new
+  end
+
+  def create
+    @expense = current_user.expenses.new(expense_params)
+    if @expense.save
+      redirect_to expenses_path, notice: I18n.t('expenses.successfuly_created')
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:name, :value, :amount, :description, :category)
+  end
 end
