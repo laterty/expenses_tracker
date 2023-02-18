6fee61fd9b369a01f1bbf0cc36cdaf94be7679e2
max <laterty@gmail.com>
2023-02-18 19:07:49 +0200

    add associations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :expenses, dependent: :destroy
+  has_many :sended_expenses, class_name: 'ExpenseSharing', foreign_key: 'sender_id', dependent: :destroy,
+                             inverse_of: :sender
+  has_many :shared_expenses, class_name: 'ExpenseSharing', foreign_key: 'recipient_id', dependent: :destroy,
+                             inverse_of: :recipient
+  has_many :followed, class_name: 'User', through: :shared_expenses, source: :sender
+  has_many :followings, class_name: 'User', through: :sended_expenses, source: :recipient
 end
