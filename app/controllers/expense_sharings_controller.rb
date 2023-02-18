547b7ab76a3d64129b12dd5f8f4b29fd11d4a852
max <laterty@gmail.com>
2023-02-18 22:23:48 +0200

    add new/create functionality to expense_sharing controller

--- app/controllers/expense_sharings_controller.rb
+++ app/controllers/expense_sharings_controller.rb
@@ -5,14 +5,23 @@ class ExpenseSharingsController < ApplicationController
 
   def index
     @shared_expenses = current_user.shared_expenses
+    @sended_expenses = current_user.sended_expenses
   end
 
   def show
     @sender = ExpenseSharing.find(params[:id]).sender
   end
 
+  def new
+    @expense_sharing = current_user.sended_expenses.new
+    @users_list = users_list_for_share
+  end
+
   def create
     @expense_sharing = current_user.sended_expenses.new(expense_sharing_params)
+    return unless @expense_sharing.save
+
+    redirect_to expenses_path, notice: I18n.t('expense_sharings.messages.successfully_shared')
   end
 
   def destroy
@@ -20,12 +29,16 @@ class ExpenseSharingsController < ApplicationController
     return unless @expense_sharing
 
     @expense_sharing.destroy
-    redirect_to expense_sharing_path, notice: I18n.t('expense_sharings.messages.successfully_destroy')
+    redirect_to expense_sharings_path, notice: I18n.t('expense_sharings.messages.successfully_destroy')
   end
 
   private
 
+  def users_list_for_share
+    User.where.not(id: current_user.followers.pluck(:id).push(current_user.id))
+  end
+
   def expense_sharing_params
-    params.permit(:expense_sharing).require(:recipient_id)
+    params.require(:expense_sharing).permit(:recipient_id)
   end
 end
