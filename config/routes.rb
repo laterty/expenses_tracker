13611ccbb538d7ce04107e6580eee114393da1ef
max <laterty@gmail.com>
2023-02-18 22:44:37 +0200

    add pritty namespace for expenses by categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  resources :categories, only: %i[show] do
+    resources :expenses, only: %i[index]
+  end
   resources :expenses
   resources :expense_sharings, except: %i[update edit]
 end
