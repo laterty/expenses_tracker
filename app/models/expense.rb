923efc69b474895353527fe70efe53763e07d69f
max <laterty@gmail.com>
2023-02-19 03:01:52 +0200

    add validations to expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Expense < ApplicationRecord
+  MIN_VALUE = 0
+
   belongs_to :user
   belongs_to :category
+
+  validates :value, presence: true, numericality: { greater_than: MIN_VALUE }
 end
