1126bfb9f3ad8a86696adc6faa480bcc0d441204
max <laterty@gmail.com>
2023-02-19 02:07:08 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+user1 = FactoryBot.create(:user, email: 'yolo@yolo.com', password: 'Yolo123*')
+user2 = FactoryBot.create(:user, email: 'polo@polo.com', password: 'Polo123*')
+
+%w[Traveling Clothing Taxi Cafes Shops Other].each do |name|
+  category = FactoryBot.create(:category, name:)
+  FactoryBot.create_list(:expense, rand(1..9), category:, user: user1)
+  FactoryBot.create_list(:expense, rand(1..9), category:, user: user2)
+end
